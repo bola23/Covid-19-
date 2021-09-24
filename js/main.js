746124828a4050ccc89900a39e16b533e32a38a0
bola23 <bolaadel28@gmai.com>
2021-09-24 05:33:58 +0200

    first verison'

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var scroll = new SmoothScroll('a[href*="#"]', {
+	speed: 500
+});
+//_________ nice Scroll____________
+$(function() {  
+    $("body").niceScroll();
+});
+
+
+
+let protectBtn = document.getElementsByClassName('how-to-protect-btn')
+let questionBtn = document.getElementsByClassName('btn question-btn')
+
+
+console.log(protectBtn)
+console.log(questionBtn)
+
+
+protectBtn.onclick=function(){ 
+ 
+
+
+}
